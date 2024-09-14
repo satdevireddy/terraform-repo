5bf4b69a0e6ad729eba46e89e055195122fa4718
satdevireddy <satyadevireddy@gmail.com>
2024-09-14 16:55:36 -0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,11 @@
+ 
+
+terraform {
+  backend "s3" {
+    bucket         = "dsp-terraform-state-backend1"
+    key            = "network/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-state"
+
+  }
+} 
